e3018fd8d4d166fffc0aa3b09fa11f22c5029a52
ansaws <38837666+ansaws@users.noreply.github.com>
2019-01-26 13:50:46 -0600

    Update move_create.py

--- Lib/move_create.py
+++ Lib/move_create.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python
+import os, sys
+from wallaby import *
 
+def forward(speed, dist):
+	set_create_distance(0)
+	create_drive_direct(speed, speed)
+  	while get_create_distance() < dist:
+		pass
+ 	create_drive_direct(0,0)

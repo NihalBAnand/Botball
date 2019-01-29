47dd4f80499e6ecdb787293cfcd0918747f957fb
ansaws <38837666+ansaws@users.noreply.github.com>
2019-01-29 17:02:42 -0600

    Update move_create.py

--- Lib/move_create.py
+++ Lib/move_create.py
@@ -8,3 +8,31 @@ def forward(speed, dist):
   	while get_create_distance() < dist:
 		pass
  	create_drive_direct(0,0)
+
+def backward(speed, dist):
+	set_create_distance(0)
+	create_drive_direct(-speed, -speed)
+  	while get_create_distance() > -dist:
+		pass
+ 	create_drive_direct(0,0)
+         
+ 	
+def spin_CCW(speed, angle):
+	set_create_total_angle(0)
+	create_spin_CCW(speed)
+	while get_create_total_angle() < angle:
+		pass
+	create_drive_direct(0,0)
+	
+def spin_CW(speed, angle):
+	set_create_total_angle(0)
+	create_spin_CW(speed)
+	while get_create_total_angle() > -angle:
+		pass
+	create_drive_direct(0,0)
+            
+def drive_to_bump(speed):
+	create_drive_direct(speed,speed)
+	while get_create_lbump() == 0 and get_create_rbump()==0:
+		pass
+	create_drive_direct(0,0)

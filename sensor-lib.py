ccdf02ebafabb425120f09291bd04561b52be835
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2019-02-02 10:47:56 -0600

    Update sensor-lib.py

--- sensor-lib.py
+++ sensor-lib.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
 import os, sys
 from wallaby import *
+from move_wallaby import *
+
+def center_color(channel):
+	camera_open_black();
+	camera_update();
+	x = get_object_center_x(channel, 0);
+	while x < 60 or x > 100:
+		camera_update();
+		x = get_object_center_x(channel, 0);
+		if x < 60:
+			spin_right(100, 100);
+		elif x > 100:
+			spin_left(100, 100);
+	ao();
+
 def analog_average(port, reads):
 	total = 0;
 	for reading in range(reads):
